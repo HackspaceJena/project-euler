eae9b67bacc3d3c65be201b2d647f114b3e3eb63
Lowl3v3l <lowl3v3l@yahoo.de>
2017-06-06 20:44:05 +0200

    deuglyfied racket solution for problem 1

--- 1/euler1.rkt
+++ 1/euler1.rkt
@@ -1,10 +1,7 @@
-#lang racket
+#lang typed/racket
 
-(define (multiples limit)
-  (foldl + 0 (filter (lambda (x) (cond
-                      [(= 0 (remainder x 3)) #t]
-                      [(= 0 (remainder x 5)) #t]
-                      (else #f)))
-        (range limit))))
-
-(multiples 1000)
+(foldl (λ ((x : Nonnegative-Integer)
+           (y : Nonnegative-Integer))
+         (if (or (zero? (remainder x 3))
+                 (zero? (remainder x 5)))
+             (+ x y) y)) 0 (range 1 1000))
