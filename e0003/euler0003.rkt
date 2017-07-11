7cf16ea48b2f48c1fecd16aeae09f254b616aeda
Lowl3v3l <lowl3v3l@yahoo.de>
2017-07-12 00:42:46 +0200

    typed racket solution for euler 3

--- e0003/euler0003.rkt
+++ e0003/euler0003.rkt
@@ -1,10 +1,12 @@
-#lang racket
+#lang typed/racket
 
 (require math/number-theory)
 
+(: max-prime-factor (-> Integer (U False Integer)))
 (define (max-prime-factor x)
   (if (prime? x) x
-      (for/or ((i (filter prime? (range 2 (sqrt x)))))
-        (if (zero? (remainder x i)) (max-prime-factor (quotient x i)) #f))))
+      (for/or ((i (filter prime? (range 2 (ceiling (assert (sqrt x) real?)))))
+        #:when (zero? (remainder x i)))
+        (max-prime-factor (quotient x i)))))
 
 (max-prime-factor 600851475143)
