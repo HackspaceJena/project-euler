047060dfbbf8ff4ce83f68ca94518784b5ad26bf
Felix Cremer <felix.cremer@uni-jena.de>
2017-08-09 12:52:06 +0200

    Change to the actual test number

--- e0010/euler0010.jl
+++ e0010/euler0010.jl
@@ -3,12 +3,11 @@ import Primes
 function sumprimes(n::Int)
   #primnumbers = SharedArray{Int}(n)
   primesum=2
-  parasum += @parallel (+) for i in 3:2:n
+  parasum = @parallel (+) for i in 3:2:n
     Primes.isprime(i)*i
   end
-  primesum
+  primesum+=parasum
 end
-sumprimes(11)
-print(2)
-@time prime = sumprimes(7)
+sumprimes(10)
+@time prime = sumprimes(2000000)
 print(prime)
